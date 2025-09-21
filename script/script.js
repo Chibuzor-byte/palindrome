650324f75867f61eac1f2690a282e3039f4a42c4
Osinachi Chibuzor <70204435+Chibuzor-byte@users.noreply.github.com>
2025-09-21 05:05:58 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,29 @@
+const checkButton = document.getElementById("check-btn");
+const textInput = document.getElementById("text-input");
+const result = document.getElementById("result");
+
+const isPalindrome = (str) => {
+  // Remove non-alphanumeric characters and lowercase
+  let cleaned = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  let reversed = cleaned.split("").reverse().join("");
+  return cleaned === reversed;
+};
+
+checkButton.addEventListener("click", () => {
+  let value = textInput.value;
+ 
+
+  if (value === "") {
+    alert("Please input a value");
+    return;
+  }
+
+  if (isPalindrome(value)) {
+    result.style.display = "block";
+    result.textContent = `${value} is a palindrome.`;
+  } else {
+    result.style.display = "block";
+    result.textContent = `${value} is not a palindrome.`;
+  }
+});
 
